3d19318b5b6af016a93d70bc70bf5769fc1e30ae
Lucas.van.der.Kleij <lucas.van.der.kleij@rabobank.nl>
2022-09-08 20:32:34 +0200

    --

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-docker build -f frontend/dely/dockerfiles/Dockerfile.dev -t lakleij/deli-app ./frontend/dely
+docker build -f frontend/dely/dockerfiles/Dockerfile.dev -t lakleij/dely-app ./frontend/dely
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push lakleij/dely-app

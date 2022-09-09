a71b81c3e02c674bec0117f32577432b2bd5c364
Lucas.van.der.Kleij <lucas.van.der.kleij@rabobank.nl>
2022-09-09 08:32:15 +0200

    pushes all tags docker

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -f frontend/dely/dockerfiles/Dockerfile.prod -t lakleij/dely-app:latest -t lakleij/dely-app:$SHA ./frontend/dely
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push lakleij/dely-app
+docker push lakleij/dely-app --all-tags

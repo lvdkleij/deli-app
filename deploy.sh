3c2039b6fbc0960d257633f24109b768c9b249b2
Lucas.van.der.Kleij <lucas.van.der.kleij@rabobank.nl>
2022-09-17 21:23:31 +0200

    test aws

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
-docker build -f frontend/dely/dockerfiles/Dockerfile.prod -t lakleij/dely-app:latest ./frontend/dely
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker push lakleij/dely-app:latest
 
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+
+docker build -f frontend/dely/dockerfiles/Dockerfile.prod -t lakleij/dely-app:latest -t 986267899441.dkr.ecr.eu-west-1.amazonaws.com/public.ecr.aws/v1v2g3g9/lakleij:latest ./frontend/dely
+
+docker push lakleij/dely-app:latest
+docker push 986267899441.dkr.ecr.eu-west-1.amazonaws.com/public.ecr.aws/v1v2g3g9/lakleij:latest
 #-t lakleij/dely-app:$SHA

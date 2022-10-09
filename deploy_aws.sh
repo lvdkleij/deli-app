4a7ef07c6f9dbfeffd3d9b60f9dee69a541173e7
Lucas.van.der.Kleij <lucas.van.der.kleij@rabobank.nl>
2022-10-09 08:11:34 +0200

    config changes

--- deploy_aws.sh
+++
@@ -1,8 +0,0 @@
-
-
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 986267899441.dkr.ecr.eu-west-1.amazonaws.com
-docker build -f frontend/dely/dockerfiles/Dockerfile.prod -t 986267899441.dkr.ecr.eu-west-1.amazonaws.com/docker ./frontend/dely
-docker push 986267899441.dkr.ecr.eu-west-1.amazonaws.com/docker
-
-
-aws s3 cp Dockerrun.aws.json s3://$S3_BUCKET/Dockerrun.aws.json

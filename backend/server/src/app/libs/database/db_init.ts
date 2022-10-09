703f66675d74f1a497ba9208075d2fbde7c08f19
Lucas.van.der.Kleij <lucas.van.der.kleij@rabobank.nl>
2022-10-09 08:37:03 +0200

    backend prod setup

--- backend/server/src/app/libs/database/db_init.ts
+++ backend/server/src/app/libs/database/db_init.ts
@@ -2,10 +2,10 @@ import { keys } from './keys';
 import { db_structure } from './db_structure';
 import { Pool } from 'pg';
 
-export const pgClient = new Pool({...keys});
+const pgClient = new Pool({...keys});
 pgClient.on('error', () => console.log('Lost PG connection'));
 
-export async function setupDatabase() {
+async function setupDatabase() {
     async function createTable(tableName: string) {
         return pgClient
             .query(db_structure[tableName as keyof typeof db_structure])
@@ -16,4 +16,5 @@ export async function setupDatabase() {
     }
 }
 
+export { pgClient, setupDatabase};
 
